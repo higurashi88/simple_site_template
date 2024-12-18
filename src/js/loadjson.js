332c894cfcabb4b1bab9dabd6f93da5e5ce2c7e4
higurashi88 <msippc1997@gmail.com>
2024-12-19 00:21:44 +0900

    ヘッダーとフッターの追加

--- src/js/loadjson.js
+++ src/js/loadjson.js
@@ -1,8 +1,26 @@
 fetch('config.json')
     .then(response => response.json())
     .then(data => {
-        console.log(data.site_name);
-        // ここでデータを利用する
+        const header = document.querySelector("header");
+        const footer = document.querySelector("footer");
+        let headerEle = document.getElementById("header");
+        let footerEle = document.getElementById("footer");
+
+        headerEle.textContent = data.site_name;
+        if (data.header_bg_color) {
+            header.style.backgroundColor = data.header_bg_color;
+        }
+        if (data.header_font_color) {
+            header.style.color = data.header_font_color;
+        }
+
+        footerEle.textContent = data.footer_msg;
+        if (data.footer_bg_color) {
+            footer.style.backgroundColor = data.footer_bg_color;
+        }
+        if (data.footer_font_color) {
+            footer.style.color = data.footer_font_color;
+        }
     })
     .catch(error => {
         console.error('Error:', error);
